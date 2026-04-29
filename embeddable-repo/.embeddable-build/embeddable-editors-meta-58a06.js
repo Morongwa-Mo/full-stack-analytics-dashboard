e300457be5cd17b0fb9c3634dea0c61ce782f7ad
Morongwa <morongwa.ralefeta@wimmy.com>
2026-04-29 11:26:02 +0200

    Ignore generated build files

--- embeddable-repo/.embeddable-build/embeddable-editors-meta-58a06.js
+++
@@ -1,9 +0,0 @@
-
-globalThis.__EMBEDDABLE__ = globalThis.__EMBEDDABLE__ || {};
-globalThis.__EMBEDDABLE__["58a06"] = globalThis.__EMBEDDABLE__["58a06"] || {};
-globalThis.__EMBEDDABLE__["58a06"].editorsMeta = globalThis.__EMBEDDABLE__["58a06"].editorsMeta || [
-  {"name":"ColorEditor","label":"Color editor","type":"color"},
-{"name":"MarkdownEditor","label":"Markdown editor","type":"markdown"}
-];
-globalThis.__EMBEDDABLE_BUNDLE_HASH__ = "58a06";
-        
